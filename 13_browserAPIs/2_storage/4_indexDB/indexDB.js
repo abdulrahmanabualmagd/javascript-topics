024f97b48061ac44dfd4ff88b03401b694510bdb
Abdularhman Ahmed <abdwbedoo@gmail.com>
2024-02-11 22:40:16 +0200

    Add IndexDB

--- 13_browserAPIs/2_storage/4_indexDB/indexDB.js
+++ 13_browserAPIs/2_storage/4_indexDB/indexDB.js
@@ -1,43 +1,127 @@
+export class IndexDB {
+  // -------------------------------------------------- [ Ctor ]
+  constructor(name, version, storeNames) {
+    this.dbName = name;
+    this.dbVersion = version;
+    this.storeNames = storeNames;
+    this.db = null;
+  }
 
 
-// 4. IndexedDB
+  // ------------------------------------------------- [ Methods]
+  async init() {
+    return new Promise((res, rej) => {
+      // Open or Create request
+      const request = indexedDB.open(this.dbName, this.dbVersion);
 
-// Open (or create) a database
-const request = indexedDB.open("myDatabase", 1);
+      request.onsuccess = (e) => {
+        this.db = e.target.result;
+        res("Request Connected Successfully!");
+      };
 
-// Handle database creation/upgrading
-request.onupgradeneeded = function (event) {
-  const db = event.target.result;
-  const objectStore = db.createObjectStore("store", { keyPath: "id" });
-};
+      request.onupgradeneeded = (e) => {
+        this.db = e.target.result;
 
-// Handle database opening success
-request.onsuccess = function (event) {
-  const db = event.target.result;
+        for (const storeName of this.storeNames) {
+          if (!this.db.objectStoreNames.contains(storeName)) {
+            this.db.createObjectStore(storeName, {
+              keyPath: "id",
+              autoIncrement: true,
+            });
+          }
+        }
 
-  // Start a new transaction
-  const transaction = db.transaction(["store"], "readwrite");
+        res("Database Upgraded Successfully!");
+      };
+      request.onerror = (e) => rej("Database error", e.target.errorCode);
+    });
+  }
 
-  // Get the object store
-  const objectStore = transaction.objectStore("store");
+  async add(storeName, data) {
+    if (!this.db) return Promise.resolve("Db is Not Initialized");
 
-  // Add data to the store
-  objectStore.add({ id: 1, name: "John" });
+    return new Promise((res, rej) => {
+      const transaction = this.db.transaction(storeName, "readwrite");
+      const objectStore = transaction.objectStore(storeName);
+      const request = objectStore.add(data);
 
-  // Get data from the store
-  const getRequest = objectStore.get(1);
-  getRequest.onsuccess = function (event) {
-    console.log(event.target.result);
-  };
+      request.onsuccess = (e) => res(true);
+      request.onerror = (e) => rej(false);
+    });
+  }
 
-  // Remove data from the store
-  const deleteRequest = objectStore.delete(1);
-  deleteRequest.onsuccess = function (event) {
-    console.log("Data deleted");
-  };
-};
+  async getById(storeName, key) {
+    if (!this.db) return Promise.resolve("Db is Not Initialized");
 
-// Handle database errors
-request.onerror = function (event) {
-  console.error("Database error: " + event.target.errorCode);
-};
+    return new Promise((res, rej) => {
+      const transaction = this.db.transaction(storeName, "readonly");
+      const objectStore = transaction.objectStore(storeName);
+      const request = objectStore.get(key);
+
+      request.onsuccess = (e) => res(e.target.result);
+      request.onerror = (e) => rej(false);
+    });
+  }
+
+  async getAll(storeName) {
+    if (!this.db) return Promise.resolve("Db is Not Initialized");
+
+    return new Promise((res, rej) => {
+      const transaction = this.db.transaction(storeName, "readonly");
+      const objectStore = transaction.objectStore(storeName);
+      const request = objectStore.getAll();
+
+      request.onsuccess = (e) => res(e.target.result);
+      request.onerror = (e) => rej(false);
+    });
+  }
+
+  async deleteById(storeName, id) {
+    if (!this.db) return Promise.resolve("Db is Not Initialized");
+
+    return new Promise((res, rej) => {
+      const transaction = this.db.transaction(storeName, "readwrite");
+      const objectStore = transaction.objectStore(storeName);
+      const request = objectStore.delete(id);
+
+      request.onsuccess = () => res(true);
+      request.onerror = (e) => rej(false);
+    });
+  }
+
+  async deleteAll(storeName) {
+    if (!this.db) return Promise.resolve("Db is Not Initialized");
+
+    return new Promise((res, rej) => {
+      const transaction = this.db.transaction(storeName, "readwrite");
+      const objectStore = transaction.objectStore(storeName);
+      const request = objectStore.clear();
+
+      request.onsuccess = () => res(true);
+      request.onerror = (e) => rej(false);
+    });
+  }
+
+  async close() {
+    if (!this.db) return Promise.resolve("DB is not initialized.");
+    
+    this.db.close();
+    this.db = null;
+
+    return Promise.resolve("Database connection closed successfully.");
+  }
+
+  async reset(dbName) {
+    return new Promise((resolve, reject) => {
+      const request = indexedDB.deleteDatabase(this.dbName);
+
+      request.onsuccess = () => resolve("Database deleted successfully");
+
+      request.onerror = (event) => reject(event.target.error);
+
+      request.onblocked = () =>
+        reject("Couldn't delete database due to the operation being blocked");
+    });
+  }
+  
+}
